b1a036b95587b9f341b6a1195988aa4806cbd06f
Jenelle Hanson <62299143+Elliesfireryu@users.noreply.github.com>
2021-10-29 20:00:05 -0500

    Add files via upload

--- Practice/practice.js
+++ Practice/practice.js
@@ -46,7 +46,7 @@ function insertAt(arr, index, val){
 
 
 var array = [-1, -5, -6, -3];
-console.log(insertAt(array, 7, 9));//undefined appears instead of empty
+// console.log(insertAt(array, 7, 9));undefined appears instead of empty
 //not an official issue so I am skipping.
 
 function minToFront(arr){
@@ -69,7 +69,7 @@ function minToFront(arr){
     return arr;
 }
 
-console.log(minToFront(arr));
+// console.log(minToFront(arr));
 
 //RIOT walk
 //input: array
@@ -89,3 +89,110 @@ console.log(minToFront(arr));
 //go through array and find min
 //shift everything left of min to right
 //pit min in front at arr[0]
+
+function swap(arr, a) {
+    var temp = arr[a];
+    arr[a] = arr[arr.length-(a+1)];
+    arr[arr.length-(a+1)] = temp;
+}
+
+function reverse(arr){
+    for(var i = 0; i < arr.length/2; i++){
+        swap(arr, i);
+    }
+    return arr;
+}
+
+
+console.log(reverse([1,2,3,4,5]) + " reverse");
+
+//RIOT reverse (Recap, input, output, tests, psuedocode walkthrough)
+//in-place array reverse no second array, no built in f(x)
+//input: arr
+//output: rev arr
+//test cases:
+//[1, 0, 1] -> [1, 0, 1]
+//[] -> []
+//[2, 1] -> [1, 2]
+//[1, 2, 3, 4, 5] -> [5, 4, 3, 2, 1]
+//[-1, 2, -4, 5] -> [5, -4, 2, -1]
+
+//for loop
+//arr[0] -> temp -> arr[arr.length- 1](last place) stop in the middle
+//arr[1] -> arr[arr.length- 2]
+//end loop at index arr.length/2
+
+function swapR(arr, a, b){
+    var temp = arr[a];
+    arr[a] = arr[b];
+    arr[b] = temp;
+}
+
+function rotateArr(arr, shiftBy){
+    for(var i = 0; i < arr.length; i++){
+        var newIndex = i + shiftBy;
+        if(newIndex < arr.length) {
+            swapR(arr, i, newIndex);
+            console.log(arr);
+        }
+        // if(newIndex >= arr.length-1) {
+        //     newIndex - newIndex;
+        //     swapR(arr, i, newIndex);
+        //     // console.log(arr);
+        // }
+    }
+    return arr;
+}
+
+console.log(rotateArr([0,1,2,3], 1));
+
+// input: arr and the number of indices to shift by.
+// output: arr
+// shift values in the arr by the shiftBy number the 
+// values shifted should equate wrap to the beginning of the arr and 
+// operate in place
+
+// allow negative shifts
+// minimize memory usage
+// minimize touches (idk what that means)
+
+//for loop and a swap function
+
+
+
+
+function filterRange(arr, min, max){
+    for(var i = 0; i < arr.length; i++){
+        if((arr[i] >= min) || (arr[i] <= max)){
+            
+        }
+    }
+}
+
+function concat(arr, arr1){
+    var arrEnd = (arr.length) + (arr1.length);
+    var newArr = [];
+    var j= 0;
+    for(var i = 0; i < arr.length; i++){
+        newArr[i] = arr[i];
+    }
+
+    for(var i= arr.length; i < arrEnd; i++){
+        newArr[i] = arr1[j];
+        j++;
+    }
+    return newArr;
+}
+
+// console.log(concat([1,2,3], ["hi","hey"]));
+// console.log(concat([], [1]));
+// console.log(concat([1],[]));
+// console.log(concat([],[]));
+
+//input 2 arr's
+//output 1 arr
+//goal add the contents of the 2nd arr to the contents of the 1st arr
+//test cases
+//[], [1]
+//[1], []
+//[1], [1]
